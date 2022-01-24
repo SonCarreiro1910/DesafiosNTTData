24f560ef6b37e06de5fedca80a857789810aa963
SonCarreiro1910 <81304054+SonCarreiro1910@users.noreply.github.com>
2022-01-24 17:15:55 -0300

    Add files via upload

---
+++ financys/src/main/java/trilha/back/financys/Produto.java
@@ -0,0 +1,60 @@
+package trilha.back.financys;
+
+public class Produto {
+    private long id;
+    private String name, description;
+    private double price;
+
+    public Produto() {
+    }
+
+    public Produto(long id, String name, String description, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
+
